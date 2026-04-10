94483661a59718fe9415eb6bc7991462f34a85ce
Nils Skowasch <nils_s89@gmx.de>
2026-04-10 08:36:40 +0200

    - update dependency hidapi==0.15.0 (instead of 0.14.0) - set version to 1.1.0a6

--- src/g6_cli/__init__.py
+++ src/g6_cli/__init__.py
@@ -16,7 +16,7 @@ from g6_cli.g6_spec.decoder import DecoderMode
 from g6_cli.g6_spec.recording import MicrophoneEqualizerPreset
 from g6_cli.g6_util import to_bool
 
-VERSION = '1.1.0a5'
+VERSION = '1.1.0a6'
 
 # The name of the temporary file to remember the last toggle state in. If the file could not be found. The program
 # lets the G6 to toggle to Speakers by default.
